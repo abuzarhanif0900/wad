c5956cb2a2660253825f88711f7da5adfea7926d
abuzarhanif0900 <44770507+abuzarhanif0900@users.noreply.github.com>
2018-11-30 18:55:24 +0500

    QUIZ 4 Updated

--- lectures/13/js/script.js
+++ lectures/13/js/script.js
@@ -6,19 +6,19 @@ var questions = [{
         "None of the above"],
     correctAnswer : 0
 },{
-    question : "Which of the following can't be done with client-side JavaScript?",
-    choices : [ "Validating a form",
-        "Sending a form's contents by email",
-        "Storing the form's contents to a database file on the server",
-        "None of the above"],
+    question : "Best Doodh Jalebi In Lahore?",
+    choices : [ "Select a best option",
+        "Toba Tek Singh",
+        "Model Town",
+        "Wapda Town"],
     correctAnswer : 2
 },{
-    question : "Using _______ statement is how you test for a specific condition",
+    question : "Rate ucp events :",
     choices : [ "select",
-        "if",
-        "for",
-        "none of the above"],
-    correctAnswer : 1
+        "0",
+        "5",
+        "so so"],
+    correctAnswer : 0
 }];
 
 var currentQuestion = 0;
@@ -28,11 +28,58 @@ displayCurrentQuestion();
 document.getElementById("quiz-message").style.display = 'none';
 function displayNext() {
     /*Write your code here */
+    if(currentQuestion<=3)
+    {
+        var x=document.querySelector("input[type=radio]:checked");
+        if(x==null)
+        {
+            var mess=document.getElementById("quiz-message");
+            mess.style.display="inline";
+            mess.innerText='please select an answer';
+            var ChoiceListid=document.getElementById("choice-list");
+            ChoiceListid.innerHTML='';
+        }
+        else if (currentQuestion<3){
+            if(x.id==questions[currentQuestion].correctAnswer)
+            {
+                correctAnswers++;
+            }
+            var ChoiceListid=document.getElementById("choice-list");
+            ChoiceListid.innerHTML='';
+            currentQuestion++;
+        }
+        if(currentQuestion!=3) {
+            displayCurrentQuestion();
+        }
+        else
+        {
+            displayScore();
+            var mess=document.getElementById("quiz-message");
+            mess.style.display="none";
+            currentQuestion++;
+            var Next = document.getElementById("next-btn");
+            Next.innerText='Reset';
+        }
+
+    }
+    else {
+        resetQuiz();
+
+    }
+
 }
 
 function displayCurrentQuestion() {
     /*Write your code here */
-}
+        var questionid=document.getElementById("question");
+        questionid.innerHTML='<p>'+questions[currentQuestion].question+'<p>';
+        var ChoiceListid=document.getElementById("choice-list");
+        for(var x=0;x<questions[currentQuestion].choices.length;x++)
+        {
+            ChoiceListid.innerHTML += '<li>'+'<input id="'+x+'" type="radio" name="ch">'+ questions[currentQuestion].choices[x] +'</li>';
+        }
+
+    }
 
 function resetQuiz() {
     currentQuestion = 0;
